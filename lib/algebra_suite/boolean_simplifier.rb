06b4d143aa4bc0d6acbfa8966e8c92abc78067e3
Smiles-creator <bulavinova07s@gmail.com>
2026-03-18 23:07:03 +0300

    (feat): Implement parser and simplifier,but wip

--- lib/algebra_suite/boolean_simplifier.rb
+++ lib/algebra_suite/boolean_simplifier.rb
@@ -1,8 +1,10 @@
 module AlgebraSuite
-  # Базовый класс для всех узлов
+  # Базовый класс для всех узлов синтаксического дерева (AST).
+  # Определяет общий интерфейс для упрощения выражений.
   class BooleanNode
+
     def simplify
-      self 
+      self
     end
   end
 
@@ -16,17 +18,19 @@ module AlgebraSuite
     def to_s
       @name
     end
-    
+
+   
     def ==(other)
       other.is_a?(Variable) && @name == other.name
     end
   end
 
+
   class Not < BooleanNode
     attr_reader :operand
 
     def initialize(operand)
-      raise ArgumentError, "Operand must be BooleanNode" unless operand.is_a?(BooleanNode)
+      raise ArgumentError, "Operand must be a BooleanNode" unless operand.is_a?(BooleanNode)
       @operand = operand
     end
 
@@ -37,14 +41,28 @@ module AlgebraSuite
     def ==(other)
       other.is_a?(Not) && @operand == other.operand
     end
+
+
+    def simplify
+      simplified_operand = @operand.simplify
+      
+      return simplified_operand.operand if simplified_operand.is_a?(Not)
+      
+      if simplified_operand.is_a?(Variable)
+        return Variable.new('FALSE') if simplified_operand.name == 'TRUE'
+        return Variable.new('TRUE') if simplified_operand.name == 'FALSE'
+      end
+
+      Not.new(simplified_operand)
+    end
   end
 
-
+  # Базовый класс для бинарных операций (имеют левого и правого операнда).
   class BinaryOperation < BooleanNode
     attr_reader :left, :right
 
     def initialize(left, right)
-      raise ArgumentError, "Operands must be BooleanNode" unless left.is_a?(BooleanNode) && right.is_a?(BooleanNode)
+      raise ArgumentError, "Operands must be BooleanNodes" unless left.is_a?(BooleanNode) && right.is_a?(BooleanNode)
       @left = left
       @right = right
     end
@@ -58,12 +76,191 @@ module AlgebraSuite
     def to_s
       "(#{@left} AND #{@right})"
     end
+
+    def simplify
+      l = @left.simplify
+      r = @right.simplify
+
+      return Variable.new('FALSE') if contains_false?(l, r)
+      return r if true?(l)
+      return l if true?(r)
+      return l if l == r
+      return Variable.new('FALSE') if contradiction?(l, r)
+
+      And.new(l, r)
+    end
+
+    private
+
+    def contains_false?(l, r)
+      (l.is_a?(Variable) && l.name == 'FALSE') || (r.is_a?(Variable) && r.name == 'FALSE')
+    end
+
+    def true?(node)
+      node.is_a?(Variable) && node.name == 'TRUE'
+    end
+
+    def contradiction?(l, r)
+      (l.is_a?(Not) && l.operand == r) || (r.is_a?(Not) && r.operand == l)
+    end
   end
 
- 
+
   class Or < BinaryOperation
     def to_s
       "(#{@left} OR #{@right})"
+    end
+
+    def simplify
+      l = @left.simplify
+      r = @right.simplify
+
+      return Variable.new('TRUE') if contains_true?(l, r)
+      return r if false?(l)
+      return l if false?(r)
+      return l if l == r
+      return Variable.new('TRUE') if tautology?(l, r)
+
+      Or.new(l, r)
+    end
+
+    private
+
+    def contains_true?(l, r)
+      (l.is_a?(Variable) && l.name == 'TRUE') || (r.is_a?(Variable) && r.name == 'TRUE')
+    end
+
+    def false?(node)
+      node.is_a?(Variable) && node.name == 'FALSE'
+    end
+
+    def tautology?(l, r)
+      (l.is_a?(Not) && l.operand == r) || (r.is_a?(Not) && r.operand == l)
+    end
+  end
+
+  # Парсер выражений методом рекурсивного спуска.
+  # Преобразует строку вида "A AND (B OR C)" в дерево объектов AST.
+  class Parser
+    def initialize
+      @tokens = []
+      @pos = 0
+    end
+
+    def parse(expression)
+      tokenize(expression)
+      @pos = 0
+      return nil if @tokens.empty?
+
+      result = parse_or
+
+      raise SyntaxError, "Unexpected token '#{current_token}' at position #{@pos}" if @pos < @tokens.size
+
+      result
+    end
+
+    private
+
+    def tokenize(expression)
+      @tokens = []
+      expression.scan(/\w+|[()]/) do |match|
+        token = match.upcase
+        case token
+        when 'AND', 'OR', 'NOT', 'TRUE', 'FALSE', '(', ')'
+          @tokens << token
+        else
+          @tokens << "VAR:#{token}"
+        end
+      end
+    end
+
+    def current_token
+      @tokens[@pos]
+    end
+
+    def consume(expected = nil)
+      token = current_token
+      raise SyntaxError, "Expected #{expected}, got #{token || 'END OF INPUT'}" if expected && token != expected
+      
+      @pos += 1
+      token
+    end
+
+    def parse_or
+      left = parse_and
+      while current_token == 'OR'
+        consume('OR')
+        right = parse_and
+        left = Or.new(left, right)
+      end
+      left
+    end
+
+    def parse_and
+      left = parse_not
+      while current_token == 'AND'
+        consume('AND')
+        right = parse_not
+        left = And.new(left, right)
+      end
+      left
+    end
+
+    
+    def parse_not
+      if current_token == 'NOT'
+        consume('NOT')
+        return Not.new(parse_not)
+      end
+      parse_primary
+    end
+
+    def parse_primary
+      token = current_token
+      raise SyntaxError, "Unexpected end of expression" if token.nil?
+
+      if token == '('
+        consume('(')
+        expr = parse_or
+        consume(')')
+        return expr
+      end
+
+      if token&.start_with?('VAR:')
+        name = token.sub('VAR:', '')
+        consume
+        return Variable.new(name)
+      end
+
+      if ['TRUE', 'FALSE'].include?(token)
+        consume
+        return Variable.new(token)
+      end
+
+      raise SyntaxError, "Unexpected token: #{token}"
+    end
+  end
+
+  # Исключение, возникающее при синтаксической ошибке во входном выражении
+  class SyntaxError < StandardError; end
+
+ 
+  # Принимает строку, парсит её, упрощает дерево и возвращает результат строкой.
+  class BooleanSimplifier
+    def self.simplify_expression(expression_string)
+      parser = Parser.new
+      ast = parser.parse(expression_string)
+      return "" if ast.nil?
+
+      simplified_ast = ast.simplify
+      
+      loop do
+        next_ast = simplified_ast.simplify
+        break if next_ast.to_s == simplified_ast.to_s
+        simplified_ast = next_ast
+      end
+
+      simplified_ast.to_s
     end
   end
 end
--- test/test_boolean_simplifier.rb
+++ test/test_boolean_simplifier.rb
@@ -35,3 +35,101 @@ class TestBooleanStructure < Minitest::Test
     end
   end
 end
+
+class TestBooleanParser < Minitest::Test
+  include AlgebraSuite
+
+  def test_parse_simple_var
+    parser = Parser.new
+    ast = parser.parse("A")
+    assert_instance_of Variable, ast
+    assert_equal "A", ast.to_s
+  end
+
+  def test_parse_and
+    parser = Parser.new
+    ast = parser.parse("A AND B")
+    assert_instance_of And, ast
+    assert_equal "(A AND B)", ast.to_s
+  end
+
+  def test_parse_or
+    parser = Parser.new
+    ast = parser.parse("A OR B")
+    assert_instance_of Or, ast
+    assert_equal "(A OR B)", ast.to_s
+  end
+
+  def test_parse_not
+    parser = Parser.new
+    ast = parser.parse("NOT A")
+    assert_instance_of Not, ast
+    assert_equal "(NOT A)", ast.to_s
+  end
+
+  def test_parse_precedence_and_or
+    parser = Parser.new
+    ast = parser.parse("A OR B AND C")
+    assert_instance_of Or, ast
+    assert_instance_of And, ast.right
+    assert_equal "(A OR (B AND C))", ast.to_s
+  end
+
+  def test_parse_precedence_not_and
+    parser = Parser.new
+    ast = parser.parse("NOT A AND B")
+    assert_instance_of And, ast
+    assert_instance_of Not, ast.left
+    assert_equal "((NOT A) AND B)", ast.to_s
+  end
+
+  def test_parse_parentheses
+    parser = Parser.new
+    ast = parser.parse("(A OR B) AND C")
+    assert_instance_of And, ast
+    assert_instance_of Or, ast.left
+    assert_equal "((A OR B) AND C)", ast.to_s
+  end
+  
+  def test_parse_complex
+    parser = Parser.new
+    ast = parser.parse("A AND (NOT B OR C)")
+    # Добавлена проверка результата
+    assert_equal "(A AND ((NOT B) OR C))", ast.to_s
+  end
+
+  def test_syntax_error
+    parser = Parser.new
+    assert_raises(SyntaxError) do
+      parser.parse("A AND")
+    end
+  end
+end
+
+class TestBooleanSimplification < Minitest::Test
+  include AlgebraSuite
+
+  def test_simplify_double_not
+    assert_equal "A", BooleanSimplifier.simplify_expression("NOT NOT A")
+  end
+
+  def test_simplify_and_true
+    assert_equal "A", BooleanSimplifier.simplify_expression("A AND TRUE")
+  end
+
+  def test_simplify_and_false
+    assert_equal "FALSE", BooleanSimplifier.simplify_expression("A AND FALSE")
+  end
+
+  def test_simplify_or_true
+    assert_equal "TRUE", BooleanSimplifier.simplify_expression("A OR TRUE")
+  end
+
+  def test_simplify_absorption
+    assert_equal "A", BooleanSimplifier.simplify_expression("A OR (A AND B)")
+  end
+  
+  def test_simplify_contradiction
+    assert_equal "FALSE", BooleanSimplifier.simplify_expression("A AND (NOT A)")
+  end
+end
